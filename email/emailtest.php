49b96ee0f4095e89c4b35677fcd701ad57d1d4ba
Linas <Linas@Linass-MacBook-Pro.local>
2018-05-24 14:02:24 +0300

    Add mailtest

--- email/emailtest.php
+++ email/emailtest.php
@@ -1,4 +1,30 @@
-// Email test
+<html>
+<body>
 <?php
+if (isset($_REQUEST['email']))
+//if "email" is filled out, send email
+{
+  $to      = $_REQUEST['email'];
+  $subject = "Mail test" ;
+  $message = "If you got this message, mail sending from $server is working fine :)" ;
+  $headers = 'From: webmaster@example.com' . "\r\n" .
+      'Reply-To: webmaster@example.com' . "\r\n" .
+      'X-Mailer: PHP/' . phpversion();
 
+  mail($to, $subject, $message, $headers);
+
+}
+else
+//if "email" is not filled out, display the form
+{
+        $server = $_SERVER[HTTP_HOST];
+        echo "<h1>Mail test from $server</h1>
+        <form method='post' action='" . $_SERVER['PHP_SELF'] . "'>
+        Enter Your Email: <input name='email' type='text' /><br /><br />
+        <input type='submit' name='Submit' value='Submit' />
+        </form>";
+}
 ?>
+
+</body>
+</html>
